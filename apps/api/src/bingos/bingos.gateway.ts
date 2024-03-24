13951698ff3a09b3b03943493ce16243658e012d
jenesh <jenesh@dzangolab.com>
2024-03-24 15:59:35 +0545

    feat: setup socket in api

---
+++ apps/api/src/bingos/bingos.gateway.ts
@@ -0,0 +1,41 @@
+import {
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
+import { PlayersService } from 'src/players/players.service';
+import { BingosService } from './bingos.service';
+
+@WebSocketGateway({
+  cors: {
+    origin: '*',
+  },
+})
+export class BingosGetaway {
+  @WebSocketServer()
+  server: Server;
+
+  constructor(
+    private readonly bingosService: BingosService,
+    private readonly playersService: PlayersService,
+  ) {}
+
+  @SubscribeMessage('subscribe-bingo')
+  handleSubscription(client: Socket, data: { bingoID: string }) {
+    client.join(data.bingoID);
+  }
+
+  @SubscribeMessage('join-bingo')
+  handleJoin(client: Socket, data: { playerID: string; bingoID: string }) {
+    const { bingoID, playerID } = data;
+    const bingo = this.bingosService.findOne(bingoID);
+    const newBingo = {
+      ...bingo,
+      playerIDs: [...bingo.playerIDs, playerID],
+    };
+    this.bingosService.update(newBingo);
+    const player = this.playersService.findOne(playerID);
+    client.nsp.to(data.bingoID).emit('player-joined', player);
+  }
+}
--- apps/api/src/bingos/bingos.module.ts
+++ apps/api/src/bingos/bingos.module.ts
@@ -3,11 +3,12 @@ import { Module } from '@nestjs/common';
 import { BoardsModule } from 'src/boards/boards.module';
 import { PlayersModule } from 'src/players/players.module';
 import { BingosController } from './bingos.controller';
+import { BingosGetaway } from './bingos.gateway';
 import { BingosService } from './bingos.service';
 
 @Module({
   imports: [InMemoryDBModule.forFeature('bingos'), PlayersModule, BoardsModule],
   controllers: [BingosController],
-  providers: [BingosService],
+  providers: [BingosService, BingosGetaway],
 })
 export class BingosModule {}
